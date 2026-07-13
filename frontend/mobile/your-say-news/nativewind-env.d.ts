4f2e44e76437e44ddebbfaafb3e44e43ad22f849
Max Hutchings <m.hutchings030@gmail.com>
2026-07-13 23:41:20 +0100

    Adding pre commit

--- frontend/mobile/your-say-news/nativewind-env.d.ts
+++ frontend/mobile/your-say-news/nativewind-env.d.ts
@@ -1,5 +1,7 @@
 /// <reference types="nativewind/types" />
 
+declare module "*.css";
+
 /**
  * NativeWind TypeScript declarations
  * This enables className prop type support on React Native components
